8d6dbaadb5d883337a4f7ae13f22c47e88c64334
Alex Plischke <alex.plischke@saucelabs.com>
2022-04-04 13:30:36 -0700

    Ensure all dev dependencies are present when needed

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -3,8 +3,12 @@ rm -rf ./bundle/
 set -e
 mkdir ./bundle/
 cp -r ./lib/ ./bundle/lib/
-cp -r node_modules/ ./bundle/node_modules/
 cp package.json bundle/package.json
 cp package-lock.json bundle/package-lock.json
 cp "$(which node)" bundle/
-rm -r bundle/node_modules/puppeteer/.local-chromium
+
+pushd bundle/
+npm cache clean --force
+npm ci --production
+rm -r node_modules/puppeteer/.local-chromium
+popd
