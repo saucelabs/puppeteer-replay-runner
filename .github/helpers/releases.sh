ba33c4f9b5ce50a93402846de9671242e8644ec6
Alex Plischke <alex.plischke@saucelabs.com>
2022-04-05 16:45:15 -0700

    Setup Release Pipeline (#2)
    * Setup Release Pipeline

---
+++ .github/helpers/releases.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+function get_release_id() {
+    i=0
+    while [ $i -lt 3 ]
+    do
+        i=`expr $i + 1`
+        RELEASE_ID=$(curl -s -H "Authorization: token $1" \
+            https://api.github.com/repos/$2/releases | \
+            jq ".[] | select(.tag_name == \"$3\") | .id")
+        if [ -z "$RELEASE_ID" ]; then
+            sleep 5
+        else
+            echo $RELEASE_ID
+            return 0
+        fi
+    done
+    return 1
+}
