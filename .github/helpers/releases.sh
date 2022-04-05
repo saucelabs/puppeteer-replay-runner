84be9fc1e3a259cc4a3147d60c02a95e0d861b9a
Alex Plischke <alex.plischke@saucelabs.com>
2022-04-05 16:11:41 -0700

    Relocate var increment for safety

--- .github/helpers/releases.sh
+++ .github/helpers/releases.sh
@@ -4,11 +4,11 @@ function get_release_id() {
     i=0
     while [ $i -lt 3 ]
     do
+        i=`expr $i + 1`
         RELEASE_ID=$(curl -s -H "Authorization: token $1" \
             https://api.github.com/repos/$2/releases | \
             jq ".[] | select(.tag_name == \"$3\") | .id")
         if [ -z "$RELEASE_ID" ]; then
-            i=`expr $i + 1`
             sleep 5
         else
             echo $RELEASE_ID
