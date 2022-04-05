dbc5e1eeba7035dd3c18f492a1b90d22e28cb224
Alex Plischke <alex.plischke@saucelabs.com>
2022-04-05 14:08:51 -0700

    Poll for release ID

---
+++ .github/helpers/releases.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function get_release_id() {
+    i=0
+    while [ $i -lt 3 ]
+    do
+        RELEASE_ID=$(curl -s -H "Authorization: token $1" \
+            https://api.github.com/repos/$2/releases | \
+            jq ".[] | select(.tag_name == \"$3\") | .id")
+        if [ -z "$RELEASE_ID" ]; then
+            i=`expr $i + 1`
+            sleep 5
+        else
+            echo $RELEASE_ID
+        fi
+    done
+    return 1
+}
